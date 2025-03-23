e5e19791ad2ecf4bbdb391ed09158c999d402122
Durga-velu <durgadevi134babu@gmail.com>
2025-03-23 13:13:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,25 @@ function convertToRoman(num) {
 
 		for(let key in obj){
 				while(num>=obj[key]){
-				 ans+=key
+				 if(num>=90 || num<100){
+					 ans+="XC";
+					 num-=90
+				 }
+					 else if(num>=40 || num<50){
+						ans=ans+"XL";
+						 num-=40;
+					}
+					 
+					 else if(num===9){
+						ans=ans+"IX";
+						 num-=9
+					}
+					else if(num===4){
+						ans=ans+"IV"
+						num-=4;
+					}
+					
+				ans+=key
 				num-=obj[i]
 			}
 		}
@@ -23,7 +41,7 @@ return ans
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
- 
+ let num=798
 console.log(convertToRoman(num))
 
 
